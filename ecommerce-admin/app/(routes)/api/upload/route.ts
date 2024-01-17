1ba057bce9ec5ae82a5a1b37a8d1b23c358fd11e
Denis Corlade <corladedenis19@gmail.com>
2024-01-17 18:42:29 +0100

    Upload files to AWS S3 bucket

--- ecommerce-admin/app/(routes)/api/upload/route.ts
+++ ecommerce-admin/app/(routes)/api/upload/route.ts
@@ -1,14 +1,60 @@
 import { NextResponse } from 'next/server';
+import mime from 'mime-types';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+const bucketName = process.env.S3_BUCKET_NAME;
+
+const client = new S3Client({
+  region: process.env.S3_REGION,
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY as string,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string,
+  },
+});
+
+async function uploadFileToS3(file, fileName) {
+  const fileBuffer = file;
+  const ext = fileName.split('.').pop();
+  const newFilename = Date.now() + '.' + ext;
+
+  await client.send(
+    new PutObjectCommand({
+      Bucket: bucketName,
+      Key: newFilename,
+      Body: fileBuffer,
+      ACL: 'public-read',
+      ContentType: 'image/jpg',
+    })
+  );
+
+  return newFilename;
+}
 
 export async function POST(req: Request) {
-  const formData = await req.formData();
-  const file = formData.get('file');
+  try {
+    const formData = await req.formData();
 
-  return NextResponse.json('ok');
-  // const form = new multiparty.Form();
+    // console.log('form data', formData);
 
-  // form.parse(req as any, (err, fields, files) => {
-  //   console.log(files.length);
-  //   return NextResponse.json('ok');
-  // });
+    const links = [];
+
+    for (let [_, file] of formData) {
+      if (!file) {
+        return NextResponse.json(
+          { error: 'No files received.' },
+          { status: 400 }
+        );
+      }
+      if (file instanceof File) {
+        // console.log({ ext, file });
+        const buffer = Buffer.from(await file.arrayBuffer());
+        const filename = await uploadFileToS3(buffer, file.name);
+
+        const link = `https://${bucketName}.s3.amazonaws.com/${filename}`;
+        links.push(link);
+      }
+      return NextResponse.json({ success: true, links });
+    }
+  } catch (error) {
+    return NextResponse.json({ error: 'Error uploading file' });
+  }
 }
